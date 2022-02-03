3b7d1c6eb7dd03a7fc0eefbcfdb4603626d89b94
shinyay <shinya.com@gmail.com>
2022-02-03 19:23:17 +0900

    Add ID Annotation

--- src/main/kotlin/io/spring/shinyay/graphql/entity/Member.kt
+++ src/main/kotlin/io/spring/shinyay/graphql/entity/Member.kt
@@ -1,3 +1,13 @@
 package io.spring.shinyay.graphql.entity
 
-data class Member(val id: Int, val name: String)
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+
+@Entity
+data class Member(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Int,
+    val name: String)
