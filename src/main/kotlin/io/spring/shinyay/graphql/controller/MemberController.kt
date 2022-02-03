c76e5172d54fe9144eacc9d54b6f170c7e4b73a1
shinyay <shinya.com@gmail.com>
2022-02-03 18:47:46 +0900

    Add Query for Member

--- src/main/kotlin/io/spring/shinyay/graphql/controller/MemberController.kt
+++ src/main/kotlin/io/spring/shinyay/graphql/controller/MemberController.kt
@@ -1,7 +1,13 @@
 package io.spring.shinyay.graphql.controller
 
+import io.spring.shinyay.graphql.entity.Member
+import io.spring.shinyay.graphql.repository.MemberRepository
+import org.springframework.graphql.data.method.annotation.QueryMapping
 import org.springframework.stereotype.Controller
 
 @Controller
-class MemberController {
+class MemberController(val repository: MemberRepository) {
+
+    @QueryMapping
+    fun members(): MutableList<Member> = repository.findAll()
 }
