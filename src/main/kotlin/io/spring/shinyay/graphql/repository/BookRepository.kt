64d95cec42e48339c7dbff515c5fd919eac110ae
shinyay <shinya.com@gmail.com>
2022-06-29 06:05:19 +0900

    Modify Class name

--- src/main/kotlin/io/spring/shinyay/graphql/repository/BookRepository.kt
+++ src/main/kotlin/io/spring/shinyay/graphql/repository/BookRepository.kt
@@ -1,6 +1,5 @@
 package io.spring.shinyay.graphql.repository
 
-import io.spring.shinyay.graphql.entity.Author
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface MemberRepository : JpaRepository<Author, Int>
+interface BookRepository : JpaRepository<Author, Int>
