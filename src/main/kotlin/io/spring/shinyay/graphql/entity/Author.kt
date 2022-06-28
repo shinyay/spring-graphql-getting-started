3bb678e0147fb40dee1e9fecc05d4ac80401d528
shinyay <shinya.com@gmail.com>
2022-06-29 06:38:59 +0900

    Add Author for JPA Entity

--- src/main/kotlin/io/spring/shinyay/graphql/entity/Author.kt
+++ src/main/kotlin/io/spring/shinyay/graphql/entity/Author.kt
@@ -1,3 +1,12 @@
 package io.spring.shinyay.graphql.entity
 
-data class Author()
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.Id
+
+@Entity
+data class Author(
+    @Id
+    @GeneratedValue
+    val id: Long
+)
