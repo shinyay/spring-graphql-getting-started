1ccae8f95944d4a79f6078c4d246811050e773e0
shinyay <shinya.com@gmail.com>
2022-06-29 06:06:00 +0900

    Midify Entity name

--- src/main/kotlin/io/spring/shinyay/graphql/repository/BookRepository.kt
+++ src/main/kotlin/io/spring/shinyay/graphql/repository/BookRepository.kt
@@ -1,5 +1,6 @@
 package io.spring.shinyay.graphql.repository
 
+import io.spring.shinyay.graphql.entity.Book
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface BookRepository : JpaRepository<Author, Int>
+interface BookRepository : JpaRepository<Book, Int>
