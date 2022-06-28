b55fe67329495d99471978a8ec3e0c53ab4a293d
shinyay <shinya.com@gmail.com>
2022-06-28 11:41:26 +0900

    Add Generation Type

--- src/main/kotlin/io/spring/shinyay/graphql/entity/Book.kt
+++ src/main/kotlin/io/spring/shinyay/graphql/entity/Book.kt
@@ -2,11 +2,12 @@ package io.spring.shinyay.graphql.entity
 
 import javax.persistence.Entity
 import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
 import javax.persistence.Id
 
 @Entity
 data class Book (
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Long
     )
