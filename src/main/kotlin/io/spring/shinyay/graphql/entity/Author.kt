dd31485a71be47f1b68fa318097f014849a9e5bf
shinyay <shinya.com@gmail.com>
2022-06-29 06:46:30 +0900

    Add one to many relationship

--- src/main/kotlin/io/spring/shinyay/graphql/entity/Author.kt
+++ src/main/kotlin/io/spring/shinyay/graphql/entity/Author.kt
@@ -1,12 +1,13 @@
 package io.spring.shinyay.graphql.entity
 
-import javax.persistence.Entity
-import javax.persistence.GeneratedValue
-import javax.persistence.Id
+import javax.persistence.*
 
 @Entity
 data class Author(
     @Id
     @GeneratedValue
-    val id: Long
+    val id: Long,
+    val name: String,
+    @OneToMany(cascade = [CascadeType.ALL], fetch = FetchType.LAZY)
+    val books: List<Book>
 )
