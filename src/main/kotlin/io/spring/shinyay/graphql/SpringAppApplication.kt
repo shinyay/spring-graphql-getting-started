fed4d4e2982db41cacd15f6f1db9b96ebd93814e
shinyay <shinya.com@gmail.com>
2022-06-30 06:21:25 +0900

    Add Application Runner

--- src/main/kotlin/io/spring/shinyay/graphql/SpringAppApplication.kt
+++ src/main/kotlin/io/spring/shinyay/graphql/SpringAppApplication.kt
@@ -1,10 +1,19 @@
 package io.spring.shinyay.graphql
 
+import io.spring.shinyay.graphql.repository.AuthorRepository
+import io.spring.shinyay.graphql.repository.BookRepository
+import org.springframework.boot.ApplicationRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
-class SpringAppApplication
+class SpringAppApplication {
+
+	@Bean
+	fun applicationRunner(authorRepository: AuthorRepository, bookRepository: BookRepository): ApplicationRunner {
+	}
+}
 
 fun main(args: Array<String>) {
 	runApplication<SpringAppApplication>(*args)
