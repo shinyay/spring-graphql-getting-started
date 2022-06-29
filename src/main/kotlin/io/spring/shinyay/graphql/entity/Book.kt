a36774f4ba416937917de8f3b436808ddf0bce2b
shinyay <shinya.com@gmail.com>
2022-06-29 09:44:09 +0900

    Add initializing parameter

--- src/main/kotlin/io/spring/shinyay/graphql/entity/Book.kt
+++ src/main/kotlin/io/spring/shinyay/graphql/entity/Book.kt
@@ -3,12 +3,12 @@ package io.spring.shinyay.graphql.entity
 import javax.persistence.*
 
 @Entity
-data class Book (
+data class Book(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    val id: Long,
-    val title: String,
-    val publisher:String,
+    val id: Long = 0,
+    val title: String = "",
+    val publisher: String = "",
     @ManyToOne(fetch = FetchType.LAZY)
-    val author: Author
+    val author: Author? = null
     )
