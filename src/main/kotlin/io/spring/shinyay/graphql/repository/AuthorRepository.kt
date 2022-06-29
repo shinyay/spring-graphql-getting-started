bbaf3983d4c6f9fc63afbec5764e9105bfdcfd0b
shinyay <shinya.com@gmail.com>
2022-06-30 06:19:45 +0900

    Modify repository interface for Author entity

--- src/main/kotlin/io/spring/shinyay/graphql/repository/AuthorRepository.kt
+++ src/main/kotlin/io/spring/shinyay/graphql/repository/AuthorRepository.kt
@@ -1,4 +1,6 @@
 package io.spring.shinyay.graphql.repository
 
-class AuthorRepository {
-}
+import io.spring.shinyay.graphql.entity.Author
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface AuthorRepository : JpaRepository<Author, Long>
