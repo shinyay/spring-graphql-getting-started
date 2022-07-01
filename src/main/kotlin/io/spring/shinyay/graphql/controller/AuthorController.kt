15f848fa6cf590d482da1dccbb518538495366a1
shinyay <shinya.com@gmail.com>
2022-07-01 10:33:04 +0900

    Add Author Repository injected on Constructor

--- src/main/kotlin/io/spring/shinyay/graphql/controller/AuthorController.kt
+++ src/main/kotlin/io/spring/shinyay/graphql/controller/AuthorController.kt
@@ -1,7 +1,11 @@
 package io.spring.shinyay.graphql.controller
 
+import io.spring.shinyay.graphql.entity.Author
+import io.spring.shinyay.graphql.repository.AuthorRepository
+import org.springframework.graphql.data.method.annotation.QueryMapping
 import org.springframework.stereotype.Controller
 
 @Controller
-class AuthorController {
+class AuthorController(val authorRepository: AuthorRepository) {
+
 }
