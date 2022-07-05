59fdcdfb93a68d24d18e25ad5936cd3dc540c554
shinyay <shinya.com@gmail.com>
2022-07-05 11:30:05 +0900

    Add funtion for Author By ID

--- src/main/kotlin/io/spring/shinyay/graphql/controller/AuthorController.kt
+++ src/main/kotlin/io/spring/shinyay/graphql/controller/AuthorController.kt
@@ -2,12 +2,19 @@ package io.spring.shinyay.graphql.controller
 
 import io.spring.shinyay.graphql.entity.Author
 import io.spring.shinyay.graphql.repository.AuthorRepository
+import org.springframework.graphql.data.method.annotation.Argument
 import org.springframework.graphql.data.method.annotation.QueryMapping
 import org.springframework.stereotype.Controller
+import java.util.*
 
 @Controller
 class AuthorController(val authorRepository: AuthorRepository) {
 
     @QueryMapping
     fun authors(): Iterable<Author> = authorRepository.findAll()
+
+    @QueryMapping
+    fun authorById(@Argument id: Long): Optional<Author> {
+        return authorRepository.findById(id)
+    }
 }
