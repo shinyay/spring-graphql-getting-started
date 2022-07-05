fba56f85b532a5bf177457ad4f2846d54dd8ace0
shinyay <shinya.com@gmail.com>
2022-07-05 11:23:55 +0900

    Modify code syntax to optimize

--- src/main/kotlin/io/spring/shinyay/graphql/entity/Book.kt
+++ src/main/kotlin/io/spring/shinyay/graphql/entity/Book.kt
@@ -9,11 +9,15 @@ data class Book(
     val id: Long = 0,
     val title: String = "",
     val publisher: String = "",
-    ) {
+) {
     @ManyToOne(fetch = FetchType.LAZY)
     var author: Author? = null
 
-    constructor(id: Long = 0, title: String = "", publisher: String = "", author: Author? = null) : this(id, title, publisher) {
+    constructor(id: Long = 0, title: String = "", publisher: String = "", author: Author? = null) : this(
+        id,
+        title,
+        publisher
+    ) {
         this.author = author
     }
 }
