04a04301808582f0b3bf1cdff7462b17e26aa355
shinyay <shinya.com@gmail.com>
2022-07-05 11:46:23 +0900

    Add data class for Mutation

--- src/main/kotlin/io/spring/shinyay/graphql/controller/AuthorController.kt
+++ src/main/kotlin/io/spring/shinyay/graphql/controller/AuthorController.kt
@@ -3,6 +3,7 @@ package io.spring.shinyay.graphql.controller
 import io.spring.shinyay.graphql.entity.Author
 import io.spring.shinyay.graphql.repository.AuthorRepository
 import org.springframework.graphql.data.method.annotation.Argument
+import org.springframework.graphql.data.method.annotation.MutationMapping
 import org.springframework.graphql.data.method.annotation.QueryMapping
 import org.springframework.stereotype.Controller
 import java.util.*
@@ -17,4 +18,11 @@ class AuthorController(val authorRepository: AuthorRepository) {
     fun authorById(@Argument id: Long): Optional<Author> {
         return authorRepository.findById(id)
     }
+
+    @MutationMapping
+    fun addBook(@Argument book: BookInput) {
+
+    }
 }
+
+data class BookInput(val title: String, val publisher: String, val authorId: Long)
