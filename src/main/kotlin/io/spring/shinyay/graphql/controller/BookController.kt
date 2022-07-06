fa15be04b24f65edb6379fedd9af403f4734e902
shinyay <shinya.com@gmail.com>
2022-07-06 22:56:59 +0900

    Add SchemaMapping for Query type

--- src/main/kotlin/io/spring/shinyay/graphql/controller/BookController.kt
+++ src/main/kotlin/io/spring/shinyay/graphql/controller/BookController.kt
@@ -2,11 +2,13 @@ package io.spring.shinyay.graphql.controller
 
 import io.spring.shinyay.graphql.entity.Book
 import io.spring.shinyay.graphql.repository.BookRepository
+import org.springframework.graphql.data.method.annotation.SchemaMapping
 import org.springframework.stereotype.Controller
 
 @Controller
 class BookController(val bookRepository: BookRepository) {
 
+    @SchemaMapping(typeName = "Query")
     fun findAllBooks(): MutableList<Book> {
         return bookRepository.findAll()
     }
