0db613b1788003ff39c86d7878b8fa28f168c44a
shinyay <shinya.com@gmail.com>
2022-07-06 22:55:29 +0900

    Add find all books function

--- src/main/kotlin/io/spring/shinyay/graphql/controller/BookController.kt
+++ src/main/kotlin/io/spring/shinyay/graphql/controller/BookController.kt
@@ -1,8 +1,13 @@
 package io.spring.shinyay.graphql.controller
 
+import io.spring.shinyay.graphql.entity.Book
 import io.spring.shinyay.graphql.repository.BookRepository
 import org.springframework.stereotype.Controller
 
 @Controller
 class BookController(val bookRepository: BookRepository) {
+
+    fun findAllBooks(): MutableList<Book> {
+        return bookRepository.findAll()
+    }
 }
