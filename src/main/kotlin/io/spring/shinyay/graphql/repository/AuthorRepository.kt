4c9419fca820d3735bac52f0b1f9e3b193a3549e
shinyay <shinya.com@gmail.com>
2022-07-06 23:06:22 +0900

    Add custom function

--- src/main/kotlin/io/spring/shinyay/graphql/repository/AuthorRepository.kt
+++ src/main/kotlin/io/spring/shinyay/graphql/repository/AuthorRepository.kt
@@ -3,4 +3,7 @@ package io.spring.shinyay.graphql.repository
 import io.spring.shinyay.graphql.entity.Author
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface AuthorRepository : JpaRepository<Author, Long>
+interface AuthorRepository : JpaRepository<Author, Long> {
+
+    fun findAuthorByName(): List<Author>
+}
