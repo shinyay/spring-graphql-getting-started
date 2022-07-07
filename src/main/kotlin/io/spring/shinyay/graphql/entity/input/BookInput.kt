77fea9af0b981390e5f31898d50cbe2057dba672
shinyay <shinya.com@gmail.com>
2022-07-08 08:54:05 +0900

    Modify Input model using Author name instead of Author ID

--- src/main/kotlin/io/spring/shinyay/graphql/entity/input/BookInput.kt
+++ src/main/kotlin/io/spring/shinyay/graphql/entity/input/BookInput.kt
@@ -1,3 +1,3 @@
 package io.spring.shinyay.graphql.entity.input
 
-data class BookInput(val title: String, val publisher: String, val authorId: Long)
+data class BookInput(val title: String, val publisher: String, val authorName: String)
