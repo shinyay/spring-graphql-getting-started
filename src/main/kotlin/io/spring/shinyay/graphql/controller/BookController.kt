1ea23fbd8ddeaa6a6a33c0b8e497d0356caf52b9
shinyay <shinya.com@gmail.com>
2022-07-08 09:12:30 +0900

    Modify typo from SchemaMapping to QueryMapping

--- src/main/kotlin/io/spring/shinyay/graphql/controller/BookController.kt
+++ src/main/kotlin/io/spring/shinyay/graphql/controller/BookController.kt
@@ -1,15 +1,22 @@
 package io.spring.shinyay.graphql.controller
 
+import io.spring.shinyay.graphql.entity.Author
 import io.spring.shinyay.graphql.entity.Book
+import io.spring.shinyay.graphql.entity.input.BookInput
 import io.spring.shinyay.graphql.repository.BookRepository
+import org.springframework.graphql.data.method.annotation.Argument
+import org.springframework.graphql.data.method.annotation.MutationMapping
+import org.springframework.graphql.data.method.annotation.QueryMapping
 import org.springframework.graphql.data.method.annotation.SchemaMapping
 import org.springframework.stereotype.Controller
 
 @Controller
 class BookController(val bookRepository: BookRepository) {
 
-    @SchemaMapping(typeName = "Query", value = "books")
+    @QueryMapping
     fun findAllBooks(): MutableList<Book> {
         return bookRepository.findAll()
     }
+
+
 }
